8492733a42e17f28473fa0189a9b21df211beb40
ld-oliveira <ld-oliveira@uol.com.br>
2023-09-26 07:17:12 -0300

    primeiro commit

---
+++ calculadora.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main() 
+{
+
+char  operacao;
+double numero1, numero2, resultado;
+
+
+    printf ("Coe, testa essa calc ae\n");
+    printf ("qual operacao voce quer fazer? (+, -, *, ou /)\n");
+    scanf ("%c", &operacao);
+
+
+    printf ("Digite o primeiro numero:\n");
+    scanf ("%lf", &numero1);
+
+    printf ("Digite o segundo numero:\n");
+    scanf ("%lf", &numero2);
+
+switch (operacao)
+{
+case '+':
+    resultado = numero1 + numero2;
+    break;
+case '-':
+    resultado = numero1 - numero2;
+    break;
+case '*':
+    resultado = numero1 * numero2;
+    break;
+
+case '/':
+    if (numero2 == 0){
+        printf("divisao por 0 nao e permitida");
+        return 1;
+    } else {
+        resultado = numero1 / numero2;
+    break;
+
+    }
+    
+    
+
+default:
+printf("erro: voce escreveu uma operacao invalida");
+return 1;
+}
+
+printf("%.2lf, %c, %.2lf, = %.2lf",  numero1, operacao, numero2, resultado);
+    return 0;
+}
